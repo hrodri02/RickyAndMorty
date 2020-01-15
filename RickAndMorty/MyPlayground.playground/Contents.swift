9b9c7eb6a172861057758f35928d674aa001d96a
Eri <eri@Brayans-MacBook-Air.local>
2020-01-15 09:49:43 -0800

    added playground to test out code

--- RickAndMorty/MyPlayground.playground/Contents.swift
+++ RickAndMorty/MyPlayground.playground/Contents.swift
@@ -1,3 +1,14 @@
 import UIKit
 
-var str = "Hello, playground"
+let celcius = [-5.0, 10.0, 21.0, 33.0, 50.0]
+var fahrenheit: [Double] = celcius.map { $0 * (9.0 / 5.0) + 32 }
+print(celcius)
+print(fahrenheit)
+
+let values = [7.0, 3.0, 10.0]
+let avg: Double = values.reduce(0.0) { $0 + $1 / Double(values.count) }
+print(avg)
+
+let arr = [11, 13, 14, 17, 21, 23, 33, 22]
+let even = arr.filter { $0 % 2 == 0 }
+print(even)
