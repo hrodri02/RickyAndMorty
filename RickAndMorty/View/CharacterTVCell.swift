cc9e93af82a347e2c04ef64948425d27736daeac
Eri <eri@Brayans-MacBook-Air.local>
2020-01-14 19:44:01 -0800

    custom table view cell to display rick and morty characters

--- RickAndMorty/View/CharacterTVCell.swift
+++ RickAndMorty/View/CharacterTVCell.swift
@@ -6,4 +6,77 @@
 //  Copyright © 2020 Eri. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class CharacterTVCell: UITableViewCell
+{
+    let characterImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.layer.cornerRadius = 20.0
+        imageView.layer.masksToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "AvenirNext-Medium", size: 14.0)
+        label.textColor = UIColor.black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let dateLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "AvenirNext-Regular", size: 12.0)
+        label.textColor = UIColor.black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        addSubviews()
+        setupUiComponents()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func addSubviews() {
+        addSubview(characterImageView)
+        addSubview(nameLabel)
+        addSubview(dateLabel)
+    }
+    
+    private func setupUiComponents() {
+        setCharacterImageViewConstraints()
+        setNameLabelConstraints()
+        setDateLabelConstraints()
+    }
+    
+    private func setCharacterImageViewConstraints() {
+        NSLayoutConstraint.activate([
+            characterImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10.0),
+            characterImageView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            characterImageView.heightAnchor.constraint(equalToConstant: 40.0),
+            characterImageView.widthAnchor.constraint(equalToConstant: 40.0)
+            ])
+    }
+    
+    private func setNameLabelConstraints() {
+        NSLayoutConstraint.activate([
+            nameLabel.leadingAnchor.constraint(equalTo: characterImageView.trailingAnchor, constant: 10.0),
+            nameLabel.bottomAnchor.constraint(equalTo: characterImageView.centerYAnchor, constant: -2.5)
+            ])
+    }
+    
+    private func setDateLabelConstraints() {
+        NSLayoutConstraint.activate([
+            dateLabel.leadingAnchor.constraint(equalTo: characterImageView.trailingAnchor, constant: 10.0),
+            dateLabel.topAnchor.constraint(equalTo: characterImageView.centerYAnchor, constant: 2.5)
+            ])
+    }
+}
