4162c7b23514edfcc56e22644353337533d7091c
Eri <eri@Brayans-MacBook-Air.local>
2020-01-14 19:44:36 -0800

    added functions for coverting dates to strings

--- RickAndMorty/Extensions/Date+Ext.swift
+++ RickAndMorty/Extensions/Date+Ext.swift
@@ -6,4 +6,27 @@
 //  Copyright © 2020 Eri. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension Date {
+    func getHourMinSecStr() -> String {
+        let date = self
+        let calendar = Calendar.current
+        
+        let hour = calendar.component(.hour, from: date)
+        let minutes = calendar.component(.minute, from: date)
+        let seconds = calendar.component(.second, from: date)
+        
+        let minStr = (minutes < 10) ? "0\(minutes)" : "\(minutes)"
+        let secStr = (seconds < 10) ? "0\(seconds)" : "\(seconds)"
+        
+        return "\(hour):\(minStr):\(secStr)"
+    }
+    
+    func getDateAsString(format: String) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = format
+        let dateStr = dateFormatter.string(from: self)
+        return dateStr
+    }
+}
